4fb99bc37414b001a4a1338bc4252b11db8590f1
wra-sol <88047072+wra-sol@users.noreply.github.com>
2024-02-16 15:26:32 -0500

    initial push

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,62 @@
+import { useEffect } from "react";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
+  json,
+  useRevalidator,
+  useLoaderData,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-
+import { createSupabaseServerClient } from "./supabase.server";
+import { createBrowserClient } from "@supabase/ssr";
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
+export const loader = async ({ request }) => {
+  const env = {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  };
+  const response = new Response();
+  const { supabaseClient: supabase, headers } =
+    createSupabaseServerClient(request);
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  return json(
+    {
+      env,
+      session,
+    },
+    {
+      headers: response.headers,
+    }
+  );
+};
 export default function App() {
+  const { env, session } = useLoaderData();
+  const { revalidate } = useRevalidator();
+  const supabase = createBrowserClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
+  const serverAccessToken = session?.access_token;
+
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.access_token !== serverAccessToken) {
+        supabase.auth.getSession();
+      }
+    });
+
+    return () => {
+      data.subscription.unsubscribe();
+    };
+  }, [serverAccessToken, supabase, revalidate]);
+
   return (
     <html lang="en">
       <head>
@@ -23,7 +66,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <Outlet context={supabase} />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,31 +11,19 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Quipu</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+          <Link to={"/signin"}>
+            Sign In
+          </Link>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
+          <Link to={"/signup"}>
+            Sign Up
+          </Link>
         </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+
       </ul>
     </div>
   );
