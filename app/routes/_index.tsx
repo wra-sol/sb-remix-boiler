38a817eb5c48009d501a67b30557192abcca3afb
wra-sol <88047072+wra-sol@users.noreply.github.com>
2024-02-22 08:22:48 -0500

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,7 +11,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Quipu</h1>
+      <h1>Welcome!</h1>
       <ul>
         <li>
           <Link to={"/signin"}>
